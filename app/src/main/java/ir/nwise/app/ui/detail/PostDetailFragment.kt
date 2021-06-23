79ffdf2b809eff52c53894e87073f9173c7cb70c
Sepide <sun.vatankhah@gmail.com>
2021-06-23 22:14:26 +0200

    Added detail.

---
+++ app/src/main/java/ir/nwise/app/ui/detail/PostDetailFragment.kt
@@ -0,0 +1,40 @@
+package ir.nwise.app.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import ir.nwise.app.R
+import ir.nwise.app.databinding.FragmentPostDetailBinding
+
+class PostDetailFragment : Fragment() {
+    private lateinit var binding: FragmentPostDetailBinding
+    private val args: PostDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_post_detail, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        bindDetailInfo()
+    }
+
+    private fun bindDetailInfo() {
+        args.post?.apply {
+            with(binding) {
+                postDetailTitle.text = title
+                postDetailBody.text = body
+            }
+        }
+    }
+}
